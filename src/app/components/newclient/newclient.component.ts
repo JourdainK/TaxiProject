473a38a1b064d328656a2c10ab92dc9ac85d57e8
JourdainK <124150004+JourdainK@users.noreply.github.com>
2023-12-06 00:24:35 +0100

    add newClient -> added in DB fixed environment.development.ts fixed environment.prod.ts added to git (skipped automatic add i guess :/ )

---
+++ src/app/components/newclient/newclient.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup,  Validators} from "@angular/forms";
+import {ClientsService} from "../../services/clients.service";
+
+@Component({
+  selector: 'app-newclient',
+  templateUrl: './newclient.component.html',
+  styleUrls: ['./newclient.component.css']
+})
+export class NewclientComponent implements OnInit{
+  clientFormGroup?: FormGroup;
+  submitted = false;
+  idclient:number|null = null;
+  constructor(private fb: FormBuilder, private clientService: ClientsService) {
+  }
+
+  ngOnInit(): void {
+    this.clientFormGroup = this.fb.group({
+      mail: ["", Validators.required],
+      nomcli: ["", Validators.required],
+      prenomcli: ["", Validators.required],
+      tel: ["+32(0)", Validators.required]
+    });
+  }
+
+
+  // subscribe deprecated
+  onSaveClient() {
+    this.submitted = true;
+    if(this.clientFormGroup?.invalid) { return; }
+
+    this.clientService.createClient(this.clientFormGroup?.value).subscribe(data =>{
+      console.log(data);
+      alert("New client saved");this.idclient = data.idclient},
+        err => {
+        alert(err.headers.get("error"));
+        console.log('error')
+        });
+  }
+
+
+}
